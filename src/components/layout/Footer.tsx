3767432391b71626b645c135b08bdd1eaa66c81a
shusbox <shusbox@naver.com>
2026-02-08 10:35:47 +0900

    feat: footer 제작

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { Row, Stack, Txt } from '@/components/common';
+import { Icon, Line } from '@/styles/common';
+import { SproutIcon } from '@/asset/icons';
+import * as Styled from '@/styles/layout/footer';
+
+const Footer = () => {
+  return (
+    <Styled.Container align='center' gap='xxl'>
+      <Styled.Header>
+        <Styled.HeaderLeft align='center' gap='sm'>
+          <Icon size='small' color='primary'> <SproutIcon size={16} /> </Icon>
+          <Txt as='p'> 설스터디 </Txt>
+        </Styled.HeaderLeft>
+        <Styled.HeaderCenter gap='lg'>
+          <Txt as='p' weight='light' color='secondary'> 이용약관 </Txt>
+          <Txt as='p' weight='light' color='secondary'> 개인정보처리방침 </Txt>
+          <Txt as='p' weight='light' color='secondary'> 고객지원 </Txt>
+          <Txt as='p' weight='light' color='secondary'> 공지사항 </Txt>
+        </Styled.HeaderCenter>
+        <Styled.HeaderRight as='p' weight='light' color='secondary'> © 2024 설스터디. All rights reserved. </Styled.HeaderRight>
+      </Styled.Header>
+      <Row width='100%'> <Line /> </Row>
+      <Txt as='p' weight='light' color='secondary' align='center'>
+        설스터디는 학생과 멘토가 함께 성장하는 학습 관리 플랫폼입니다 <br />
+        문의: support@seolstudy.com | 대표전화: 02-1234-5678
+      </Txt>
+    </Styled.Container>
+  );
+}
+
+export default Footer;
---
+++ src/styles/layout/footer.ts
@@ -0,0 +1,22 @@
+'use client';
+import styled from '@emotion/styled';
+import { Row, Stack, Txt } from '@/components/common';
+import { theme } from '@/styles/theme';
+
+export const Container = styled(Stack)`
+  padding: 40px 160px;
+  border-top: 1px solid ${theme.colors.natural[200]};
+  background-color: ${theme.colors.bg['secondary']};
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const HeaderLeft = styled(Row)`justify-self: start;`;
+export const HeaderCenter = styled(Row)`justify-self: centert;`;
+export const HeaderRight = styled(Txt)`justify-self: end;`;
